579eb50a66cc829c627a8261b16b3ba8b8c6da58
Ronildo Sousa <sousaronildo400@gmail.com>
2023-04-15 21:54:51 -0300

    feat: converting a multi nivel json into a PHP enum

--- index.php
+++ index.php
@@ -6,14 +6,28 @@ use Src\Enumer;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-// $json = '{"Hearts": "H", "Diamonds": "D", "Clubs": "C", "Spades": "S"}';
-$json = file_get_contents(__DIR__ . '/teste.json');
+$json = '{"Hearts": "H", "Diamonds": "D", "Clubs": "C", "Spades": "S"}';
+// $json = file_get_contents(__DIR__ . '/teste.json');
+// $json = '[
+//     {
+//       "Rondonia": "RO"
+//     },
+//     {
+//       "Acre": "AC"
+//     },
+//     {
+//       "Amazonas": "AM"
+//     },
+//     {
+//       "Minas gerais": "MG"
+//     }
+// ]';
 $enumer = new Enumer();
-$a = $enumer->convert($json, 'src/teste.php');
-$namespace = $enumer->getNamespace($a);
-$name = $enumer->getClassName($a);
-$enum = '\\' . str_replace(';', '', $namespace) . '\\' . $name;
+$a = $enumer->convertJson($json, 'src/teste.php');
+// $namespace = $enumer->getNamespace($a);
+// $name = $enumer->getClassName($a);
+// $enum = '\\' . str_replace(';', '', $namespace) . '\\' . $name;
 
-var_dump(
-    $enum::cases()
-);
+// var_dump(
+
+// );
